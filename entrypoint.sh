180ecb5b13c0e7b5e50e6965a54e0f0aa2d63648
Sup <167943859+Ubermm@users.noreply.github.com>
2024-07-08 18:45:07 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,25 @@
+#!/bin/bash
+
 ENV_LOCAL_PATH=/app/.env.local
 
-if test -z "${DOTENV_LOCAL}" ; then
-    if ! test -f "${ENV_LOCAL_PATH}" ; then
-        echo "DOTENV_LOCAL was not found in the ENV variables and .env.local is not set using a bind volume. Make sure to set environment variables properly. "
-    fi;
+# Check if DOTENV_LOCAL is not empty and create .env.local file if necessary
+if [ -z "${DOTENV_LOCAL}" ]; then
+    if [ ! -f "${ENV_LOCAL_PATH}" ]; then
+        echo "DOTENV_LOCAL was not found in the ENV variables and .env.local is not set using a bind volume. Make sure to set environment variables properly."
+    fi
 else
     echo "DOTENV_LOCAL was found in the ENV variables. Creating .env.local file."
-    cat <<< "$DOTENV_LOCAL" > ${ENV_LOCAL_PATH}
-fi;
+    echo "${DOTENV_LOCAL}" > "${ENV_LOCAL_PATH}"
+fi
 
-if [ "$INCLUDE_DB" = "true" ] ; then
+# Start local MongoDB instance if INCLUDE_DB is true
+if [ "${INCLUDE_DB}" = "true" ]; then
     echo "Starting local MongoDB instance"
     nohup mongod &
-fi;
+fi
 
+# Export public version from package.json
 export PUBLIC_VERSION=$(node -p "require('./package.json').version")
 
+# Run dotenv to load environment variables from .env and start the Node.js server
 dotenv -e /app/.env -c -- node /app/build/index.js -- --host 0.0.0.0 --port 3000
